ec91596081fd248a53f149e1aae08197e32d2917
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-10 02:12:15 +0000

    Done.

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,8 +13,8 @@ class Patient
     @@all
   end
 
-  def new_appointment(doctor, date)
-    Appointment.new(doctor, date, self)
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
   end
 
   def appointments
